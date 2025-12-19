b329e7ddaddcb8fb646bcd716de8c9a20b4a5cf6
Cristian Tone <39335855+crisnct@users.noreply.github.com>
2025-12-19 22:56:25 +0200

    Implemented integration test for registering user (#17)

--- application/src/test/java/com/jobshunter/service/application/notifiers/EmailNotifierTest.java
+++ application/src/test/java/com/jobshunter/service/application/notifiers/EmailNotifierTest.java
@@ -1,106 +1,101 @@
 package com.jobshunter.service.application.notifiers;
 
-import com.jobshunter.database.entities.UserJobEntity;
-import com.jobshunter.database.repository.UserJobRepository;
-import com.jobshunter.service.application.UserMessagesFactory;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.security.authentication.TestingAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.test.util.ReflectionTestUtils;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.jobshunter.database.entities.RoleEntity;
+import com.jobshunter.database.entities.UserEntity;
+import com.jobshunter.database.repository.RoleRepository;
+import com.jobshunter.database.repository.UserRepository;
+import com.jobshunter.database.service.AuthService;
+import com.jobshunter.dto.RegisterRequest;
+import com.jobshunter.service.clients.SmtpMailtrapClient;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(
+    webEnvironment = WebEnvironment.RANDOM_PORT,
+    properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+            "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+            "org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration",
+        "jobshunter.useDummyData=false"
+    }
+)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class EmailNotifierTest {
 
-    @Mock
-    private JavaMailSender mailSender;
+  @Autowired
+  private MockMvc mockMvc;
 
-    @Mock
-    private UserJobRepository userJobRepository;
+  @Autowired
+  private JsonMapper mapper;
 
-    @Mock
-    private UserMessagesFactory userMessagesFactory;
+  //MOCKS -------------------------------
+  @MockitoBean
+  private UserRepository userRepository;
 
-    @Captor
-    private ArgumentCaptor<SimpleMailMessage> mailMessageCaptor;
+  @MockitoBean
+  private RoleRepository roleRepository;
+  //--------------------------------------
 
-    @Captor
-    private ArgumentCaptor<Map<String, String>> placeholdersCaptor;
+  @MockitoSpyBean
+  private AuthService authService;
 
-    private EmailNotifierService notifier;
+  @MockitoSpyBean
+  private EmailNotifierService emailNotifierService;
 
-    @BeforeEach
-    void setUp() {
-        notifier = new EmailNotifierService();
-        ReflectionTestUtils.setField(notifier, "mailSender", mailSender);
-        ReflectionTestUtils.setField(notifier, "userJobRepository", userJobRepository);
-        ReflectionTestUtils.setField(notifier, "userMessagesFactory", userMessagesFactory);
+  @MockitoSpyBean
+  private SmtpMailtrapClient smtpMailtrapClient;
 
-        SecurityContextHolder.getContext()
-                .setAuthentication(new TestingAuthenticationToken("unit_user", null));
-//        initUserJobRepo();
-//        initMailSender();
-//        initUserMsgFactory();
-//        setAuthContext();
+  @Test
+  @DisplayName("Should register user via HTTP and trigger email notifier without touching DB")
+  void shouldSendEmailWithFormattedJobs() throws Exception {
+    RegisterRequest request = new RegisterRequest(
+        "dummy.user", "test@test.com", "test1909test", "+40710221441");
 
-    }
+    UserEntity user = new UserEntity();
+    user.setUsername(request.username());
+    user.setEmail(request.email());
+    user.setVerificationToken("token-123");
 
-    @AfterEach
-    void tearDown() {
-        SecurityContextHolder.clearContext();
-    }
+    RoleEntity testRole = new RoleEntity();
+    testRole.setId(1L);
+    testRole.setName("TEST");
 
-    @DisplayName("Should send an email built from repository jobs for the authenticated user")
-    @Test
-    void shouldSendEmailWithFormattedJobs() {
-        List<UserJobEntity> jobs = List.of(job("Job1"), job("Job2"));
-        when(userJobRepository.findAllByUsernameWithUser("unit_user")).thenReturn(jobs);
-        when(userMessagesFactory.build(eq(UserMessagesFactory.MessageTemplate.JOBS_NOTIFY), anyMap()))
-                .thenReturn("formatted-body");
+    when(userRepository.existsByEmailIgnoreCase(any())).thenReturn(false);
+    when(userRepository.existsByUsernameIgnoreCase(any())).thenReturn(false);
+    when(userRepository.existsByPhoneNumberIgnoreCase(any())).thenReturn(false);
+    when(userRepository.save(any())).thenReturn(user);
+    when(roleRepository.findByName(any())).thenReturn(Optional.of(testRole));
+    doNothing().when(smtpMailtrapClient).sendEmail(anyString(), any(), anyString());
 
-        //notifier.send();
+    mockMvc.perform(post("/api/auth/register")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(request)))
+        .andExpect(status().isOk());
 
-        verify(userJobRepository).findAllByUsernameWithUser("unit_user");
-        verify(userMessagesFactory).build(eq(UserMessagesFactory.MessageTemplate.JOBS_NOTIFY),
-                placeholdersCaptor.capture());
-        Map<String, String> placeholders = placeholdersCaptor.getValue();
-        assertNotNull(placeholders.get("1"));
-        assertEquals(jobs.toString(), placeholders.get("2"));
-        assertDoesNotThrow(() -> LocalDateTime.parse(placeholders.get("1"),
-                DateTimeFormatter.ofPattern("dd-MMMM-yyyy | HH:mm", Locale.ENGLISH)));
+    verify(authService).register(request);
+    verify(emailNotifierService).sendVerificationToken(user);
+    verify(smtpMailtrapClient).sendEmail(eq(request.email()), any(), any());
+  }
 
-        verify(mailSender).send(mailMessageCaptor.capture());
-        assertEquals("formatted-body", mailMessageCaptor.getValue().getText());
-    }
-
-    private static UserJobEntity job(String jobDescription) {
-        return new UserJobEntity() {
-            @Override
-            public String toString() {
-                return jobDescription;
-            }
-        };
-    }
 }
