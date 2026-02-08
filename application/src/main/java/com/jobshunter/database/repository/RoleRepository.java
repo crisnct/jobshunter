887844f67cc07d88227be4cd2d8a55565ceb6889
VOCHIN <vochin.ionel@yahoo.com>
2026-02-08 09:06:21 +0200

    feat(cache): Configure Hibernate second-level cache for entity retrieval and cacheable queries

--- application/src/main/java/com/jobshunter/database/repository/RoleRepository.java
+++ application/src/main/java/com/jobshunter/database/repository/RoleRepository.java
@@ -4,12 +4,19 @@ import com.jobshunter.database.entities.RoleEntity;
 import com.jobshunter.processor.PackageExpected;
 import com.jobshunter.processor.SqlInjectionSafe;
 import java.util.Optional;
+
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @PackageExpected("com.jobshunter.database.service")
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
 
+  @QueryHints({
+          @QueryHint(name = org.hibernate.jpa.HibernateHints.HINT_CACHEABLE, value = "true"),
+          @QueryHint(name = org.hibernate.jpa.HibernateHints.HINT_CACHE_REGION, value = "roles.byName")
+  })
   Optional<RoleEntity> findByName(@SqlInjectionSafe String name);
 }
